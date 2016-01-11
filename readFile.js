496c6f0d1cd23c8f7bd29efdfed7bf63a369c7a2
allie jones <alliejones42@gmail.com>
2016-01-10 21:43:37 -0500

    Adding in tinyarms

--- readFile.js
+++ readFile.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 
-fs.readFile("./t0rex.txt", "UTF-8", function (err, trex) {
+fs.readFile("./trex.txt", "UTF-8", function (err, trex) {
   console.log(`File Read ${trex.length}`);
 });
-console.log(" Reading File.");
+console.log("Reading File.");
